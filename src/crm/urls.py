edf1ab3be501d698742245f6c086f4dff419e988
knuth <knuth@ethergeist.de>
2023-01-02 14:56:19 +0100

    Fix path bug

--- src/crm/urls.py
+++ src/crm/urls.py
@@ -11,19 +11,19 @@ from .views import (email_confirmed, email_not_confirmed, magic_link,
 # fmt: on
 
 urlpatterns = [
-    path("verify-email/<str:token>", verify_email, name="crm_verify_email"),
+    path("verify-email/<str:token>/", verify_email, name="crm_verify_email"),
     path(
-        "verify-email/<str:token>/success", email_confirmed, name="crm_email_confirmed"
+        "verify-email/<str:token>/success/", email_confirmed, name="crm_email_confirmed"
     ),
     path(
-        "verify-email/<str:token>/error",
+        "verify-email/<str:token>/error/",
         email_not_confirmed,
         name="crm_email_not_confirmed",
     ),
-    path("magic-link/<str:token>", magic_link, name="crm_magic_link"),
-    path("request-magic-link", request_magic_link, name="crm_request_magic_link"),
+    path("magic-link/<str:token>/", magic_link, name="crm_magic_link"),
+    path("request-magic-link/", request_magic_link, name="crm_request_magic_link"),
     path(
-        "magic-link-requested",
+        "magic-link-requested/",
         request_magic_link_submitted,
         name="crm_request_magic_link_submitted",
     ),
