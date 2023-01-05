51955edc8beb125502d94a802ef07ce7aa1bfa38
knuth <knuth@ethergeist.de>
2023-01-05 06:45:59 +0100

    Change model

--- src/crew/models/skill.py
+++ src/crew/models/skill.py
@@ -10,8 +10,12 @@ class Skill(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=255)
-    comment = models.CharField(max_length=511, null=True, blank=True)
     explanation = models.CharField(max_length=511)
+    icon = models.CharField(
+        max_length=255,
+        help_text='<a target="_blank" href="https://semantic-ui.com/elements/icon.html">Wähle ein Icon aus</a>',
+        default="heart",
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
