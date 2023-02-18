ec8ef98b8cd0caaa906b3d9008098f1af1eee4d9
knuth <knuth@ethergeist.de>
2023-02-18 13:14:18 +0100

    Bootstrap qr code display

--- src/tools/urls_shortener.py
+++ src/tools/urls_shortener.py
@@ -5,6 +5,6 @@ from django.urls import path
 from .views import display_qr_code, link_shortener
 
 urlpatterns = [
-    path("<str:slug>/", link_shortener, name="tools_link_shortener"),
-    path("<str:slug>/qr/", display_qr_code, name="tools_link_shortener_qr"),
+    path("<slug>", link_shortener, name="tools_link_shortener"),
+    path("<slug>/qr", display_qr_code, name="tools_link_shortener_qr"),
 ]
