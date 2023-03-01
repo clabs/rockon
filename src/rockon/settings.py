a1c65af3639132aee4a8226fb3ae4e9e9602d4e6
knuth <knuth@ethergeist.de>
2023-03-01 20:48:46 +0100

    use scrypt as default hasher

--- src/rockon/settings.py
+++ src/rockon/settings.py
@@ -157,6 +157,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.ScryptPasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
