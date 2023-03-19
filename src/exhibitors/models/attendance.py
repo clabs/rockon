7bd84be89e8016483aed6e0fdb7bb86f8efd1f25
knuth <knuth@ethergeist.de>
2023-03-19 15:23:21 +0100

    Fix model __str__

--- src/exhibitors/models/attendance.py
+++ src/exhibitors/models/attendance.py
@@ -18,5 +18,8 @@ class Attendance(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.day.strftime("%A, %d.%m.%Y")
+
     class Meta:
         ordering = ["day"]
