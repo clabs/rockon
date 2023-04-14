53275b59b26d7cbd23f728e4e86817a66c3b1b87
knuth <knuth@ethergeist.de>
2023-04-14 21:12:29 +0200

    Basic profile editor

--- src/api/views/__init__.py
+++ src/api/views/__init__.py
@@ -4,4 +4,5 @@ from .bandmember_signup import bandmember_signup
 from .crew_signup import crew_signup
 from .exhibitor_signup import exhibitor_signup
 from .magic_link import request_magic_link
+from .update_user_profile import update_user_profile
 from .verify_email import verify_email
