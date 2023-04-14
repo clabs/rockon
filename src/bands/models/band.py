f27420e64bc994d40cf23f30448d4a67f8d622e1
knuth <knuth@ethergeist.de>
2023-04-14 21:53:55 +0200

    Remodel bands

--- src/bands/models/band.py
+++ src/bands/models/band.py
@@ -4,6 +4,7 @@ from uuid import uuid4
 
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.crypto import get_random_string
 
 from event.models import Event
 
@@ -12,18 +13,11 @@ class Band(models.Model):
     """Band model."""
 
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    slug = models.SlugField(unique=True, default=get_random_string)
     event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="bands")
     name = models.CharField(max_length=255)
     contact = models.ForeignKey(
         User,
-        on_delete=models.CASCADE,
-        related_name="band",
-        null=True,
-        default=None,
-        blank=True,
-    )
-    slot = models.OneToOneField(
-        "TimeSlot",
         on_delete=models.CASCADE,
         related_name="band",
         null=True,
