6dbd0782a6f484a632229e01480e14ee9c2a6c8e
knuth <knuth@ethergeist.de>
2023-04-20 17:46:19 +0200

    Fix organisation model

--- src/crm/models/organisation.py
+++ src/crm/models/organisation.py
@@ -10,14 +10,18 @@ class Organisation(models.Model):
     """Organisation model."""
 
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    org_name = models.CharField(max_length=255, null=False)
-    org_address = models.CharField(max_length=511, null=True, default=None)
-    org_house_number = models.CharField(max_length=31, null=True, default=None)
-    org_address_extension = models.CharField(max_length=511, null=True, default=None)
-    org_zip = models.CharField(max_length=31, null=True, default=None)
-    org_place = models.CharField(max_length=255, null=True, default=None)
+    org_name = models.CharField(max_length=255)
+    org_address = models.CharField(max_length=511, null=True, default=None, blank=True)
+    org_house_number = models.CharField(
+        max_length=31, null=True, default=None, blank=True
+    )
+    org_address_extension = models.CharField(
+        max_length=511, null=True, default=None, blank=True
+    )
+    org_zip = models.CharField(max_length=31, null=True, default=None, blank=True)
+    org_place = models.CharField(max_length=255, null=True, default=None, blank=True)
     members = models.ManyToManyField(User, default=None, related_name="organisations")
-    internal_comment = models.TextField(null=True, default=None)
+    internal_comment = models.TextField(null=True, default=None, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
