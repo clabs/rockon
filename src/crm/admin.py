62b6610e005da0325430777526b01d191413d660
knuth <knuth@ethergeist.de>
2023-04-30 15:50:30 +0200

    Improve backstage listings

--- src/crm/admin.py
+++ src/crm/admin.py
@@ -16,6 +16,21 @@ from .models import (
 # fmt: on
 
 
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "_user",
+        "nick_name",
+        "email_is_verified",
+        "contact_mail",
+        "created_at",
+        "updated_at",
+    )
+    readonly_fields = ("user", "created_at", "updated_at")
+
+    def _user(self, obj):
+        return f"{obj.user.first_name} {obj.user.last_name}"
+
+
 class EmailVerificationAdmin(admin.ModelAdmin):
     list_display = ("user", "id", "created_at")
     readonly_fields = ("created_at", "updated_at")
@@ -31,4 +46,4 @@ admin.site.register(MagicLink, MagicLinkAdmin)
 admin.site.register(Organisation)
 admin.site.register(Sponsoring)
 admin.site.register(Type)
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, UserProfileAdmin)
