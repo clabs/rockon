772470940787631067cc98c0a22990643f2c05bf
knuth <knuth@ethergeist.de>
2023-10-30 08:07:36 +0100

    Retain imported created and modified values

--- src/bblegacy/models/event.py
+++ src/bblegacy/models/event.py
@@ -1,22 +1,30 @@
 from __future__ import annotations
 
+from datetime import datetime
+
+import pytz
 from django.db import models
 
-from bblegacy.helper import guid
+from .custom_model import CustomModel
 
 
-class Event(models.Model):
-    id = models.CharField(primary_key=True, max_length=255, default=guid)
+class Event(CustomModel):
     name = models.CharField(max_length=255)
     opening_date = models.DateTimeField()
     closing_date = models.DateTimeField()
     tracks = models.ManyToManyField("Track", related_name="events", blank=True)
 
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
-
     def __str__(self):
         return self.name
+
+    # Because we can not overwrite auto_now_add in Django
+    # https://code.djangoproject.com/ticket/16583
+    def save(self, auto_now: bool = True, *args, **kwargs):
+        if self.created is None:
+            self.created = datetime.now(pytz.utc)
+        if auto_now:
+            self.modified = datetime.now(pytz.utc)
+        super().save(*args, **kwargs)
 
     class Meta:
         ordering = ["opening_date"]
--- src/bblegacy/models/region.py
+++ src/bblegacy/models/region.py
@@ -2,15 +2,11 @@ from __future__ import annotations
 
 from django.db import models
 
-from bblegacy.helper import guid
+from .custom_model import CustomModel
 
 
-class Region(models.Model):
-    id = models.CharField(primary_key=True, max_length=255, default=guid)
+class Region(CustomModel):
     name = models.CharField(max_length=255)
-
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
