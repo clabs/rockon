7e82c4c5e2d30c80703f2d7b6378bdf9ccf585f9
ethrgeist <knuth@ethergeist.de>
2023-11-11 12:45:35 +0100

    FIx profile signals

--- src/rockon/base/models/user_profile.py
+++ src/rockon/base/models/user_profile.py
@@ -66,12 +66,12 @@ class UserProfile(CustomModel):
         return False
 
 
-# @receiver(post_save, sender=User)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         UserProfile.objects.create(user=instance)
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
 
 
-# @receiver(post_save, sender=User)
-# def save_user_profile(sender, instance, **kwargs):
-#     instance.profile.save()
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
