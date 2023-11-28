a0053d5dbd798370cf772876a556901b0d3654ab
ethrgeist <knuth@ethergeist.de>
2023-11-28 12:54:35 +0100

    Fix error no slug given when using router

--- src/rockon/library/decorators.py
+++ src/rockon/library/decorators.py
@@ -7,9 +7,12 @@ from rockon.base.models import Event
 
 def check_band_application_open(view_func):
     def _wrapped_view_func(request, *args, **kwargs):
-        event = get_object_or_404(Event, slug=kwargs["slug"])
+        slug = kwargs.get("slug", None)
+        if not slug:
+            slug = Event.objects.filter(is_current=True).first().slug
+        event = get_object_or_404(Event, slug=slug)
         if not event.band_application_open:
-            return redirect("bands:bid_closed", slug=kwargs["slug"])
+            return redirect("bands:bid_closed", slug=slug)
         return view_func(request, *args, **kwargs)
 
     return _wrapped_view_func
