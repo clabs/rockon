860e916ff6b1c3731c0e2951debb0ec9f20ec6fe
ethrgeist <knuth@ethergeist.de>
2023-11-29 15:06:00 +0100

    make current_event always available in context

---
+++ src/rockon/library/context_processors/current_event.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from rockon.base.models import Event
+
+
+def current_event(request):
+    event_id = request.session.get("current_event")
+    event = None
+    if event_id is not None:
+        try:
+            event = Event.objects.get(id=event_id)
+        except Event.DoesNotExist:
+            pass
+    return {
+        "current_event": event,
+    }
