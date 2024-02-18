2dbf90145ccf19e584f26c7e25c543415e9ee477
ethrgeist <knuth@ethergeist.de>
2024-02-18 23:59:31 +0100

    Add caching to booking_bide_overview view

--- src/rockon/bands/views/booking.py
+++ src/rockon/bands/views/booking.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+# from django.core.cache import cache
+from django.views.decorators.cache import cache_page
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.db.models import Avg, Count, Sum
 from django.http import HttpResponse
@@ -10,6 +12,7 @@ from rockon.bands.models import Band
 
 @login_required
 @user_passes_test(lambda u: u.groups.filter(name="booking").exists())
+@cache_page(60 * 5)
 def booking_bide_overview(request):
     bands = (
         Band.objects.filter(event__id=request.session["current_event"])
@@ -26,3 +29,27 @@ def booking_bide_overview(request):
         "bands": bands,
     }
     return HttpResponse(template.render(extra_context, request))
+
+# def get_data(request):
+#     data = cache.get('band_bid_results')
+#     if data is None:
+#         bands = (
+#             Band.objects.filter(event__id=request.session["current_event"])
+#             .annotate(
+#                 votes_count=Count("votes"),
+#                 votes_sum=Sum("votes__vote"),
+#                 votes_avg=Avg("votes__vote"),
+#                 vote_counters=Count("votes__vote", distinct=True)
+#             )
+#             .order_by("-votes_sum", "-votes_avg", "track")
+#         )
+#         for index, band in enumerate(bands):
+#             counters = defaultdict(int, Counter(band.votes.values_list("vote", flat=True)))
+#             if not counters:
+#                 continue
+#             for i in range(6):
+#                 counters[i]
+#             counters = dict(sorted(counters.items()))
+#             setattr(bands[index], "counters", dict(counters))
+#         cache.set('band_bid_results', bands, 60 * 5)
+#     return data
