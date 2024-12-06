2b0e821b230e83f0779b2780aeca0cd551d45553
knuth <knuth@ethergeist.de>
2024-12-06 14:05:41 +0100

    add booking admin fields

--- src/rockon/api/permissions/bands.py
+++ src/rockon/api/permissions/bands.py
@@ -27,3 +27,31 @@ class IsCrewReadOnly(permissions.BasePermission):
             and request.method in permissions.SAFE_METHODS
         ):
             return True
+
+
+class IsInGroupForFields(permissions.BasePermission):
+    """
+    Custom permission to only allow users in specific groups to update certain fields
+    """
+
+    group_name = None
+    protected_fields = []
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        if request.data:
+            has_protected_fields = any(
+                field in request.data for field in self.protected_fields
+            )
+
+            if has_protected_fields:
+                return request.user.groups.filter(name=self.group_name).exists()
+
+        return True
+
+
+class BookingFieldsPermission(IsInGroupForFields):
+    group_name = "booking"
+    protected_fields = ["track", "bid_status"]
