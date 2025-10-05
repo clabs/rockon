c129052f7a3b4b7d60ae9fbe18917167ab35a0aa
knuth <knuth@ethergeist.de>
2025-10-05 18:34:55 +0200

    mod csp

--- src/rockon/settings.py
+++ src/rockon/settings.py
@@ -72,12 +72,12 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'csp.middleware.CSPMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'csp.middleware.CSPMiddleware',
     'rockon.library.session_current_event.SessionCurrentEventMiddleware',
 ]
 
