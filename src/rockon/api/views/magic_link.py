733727bc7257d5d31126dfdaa4396f367eaf6b7d
knuth <knuth@ethergeist.de>
2025-10-05 16:23:02 +0200

    ensure lower

--- src/rockon/api/views/magic_link.py
+++ src/rockon/api/views/magic_link.py
@@ -13,11 +13,18 @@ from rockon.base.models import MagicLink
 def request_magic_link(request):
     try:
         body = json.loads(request.body)
-        user = User.objects.get(email=body.get('user_email'))
+        email = body.get('email')
+        if not email:
+            return JsonResponse(
+                {'status': 'error', 'message': 'Email is required.'}, status=400
+            )
+        user = User.objects.get(email=email.lower())
         MagicLink.create_and_send(user, make_aware(datetime.now() + timedelta(weeks=4)))
 
     except User.DoesNotExist:
-        pass
+        return JsonResponse(
+            {'status': 'error', 'message': 'User not found.'}, status=404
+        )
 
     return JsonResponse(
         {'status': 'ok', 'message': 'Magic link sent if mail matches a user.'}
