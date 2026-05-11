da3af9bc901a9f31915e123d195f9a3610fa107c
knuth <knuth@ethergeist.de>
2026-05-11 19:01:31 +0200

    remove dead code to fix async issue

--- src/rockon/settings.py
+++ src/rockon/settings.py
@@ -79,7 +79,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'rockon.library.session_current_event.SessionCurrentEventMiddleware',
 ]
 
 AUTHENTICATION_BACKENDS = [
